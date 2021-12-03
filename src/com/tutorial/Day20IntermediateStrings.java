4f388288cc22320863b7db1ef2f4f02b4f0d4386
IndranilBasu <Indranil.Basu@target.com>
2021-12-03 19:04:41 +0530

    13. Day20IntermediateStrings

--- src/com/tutorial/Day20IntermediateStrings.java
+++ src/com/tutorial/Day20IntermediateStrings.java
@@ -1,10 +1,21 @@
 package com.tutorial;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Day20IntermediateStrings {
     public void executeIntermediateStrings() {
+        //AS1 > reverseTheString()
+        String inputAS1 = "the sky is blue";
+        String outputAS1 = reverseTheString(inputAS1);
+        System.out.println("AS1 - reverseTheString :" + outputAS1);
+
+        //AS2 > simpleReverse()
+        String inputAS2 = "the sky is blue";
+        String outputAS2 = simpleReverse(inputAS2);
+        System.out.println("AS2 - simpleReverse :" + outputAS2);
+
         //AS3 > tolower()
         ArrayList<Character> inputAS3 = new ArrayList<>(Arrays.asList('S', 'c', 'A', 'l', 'e', 'r', 'A', 'c', 'a', 'D', 'e', 'm', 'y'));
         ArrayList<Character> outputAS3 = to_lower(inputAS3);
@@ -22,6 +33,78 @@ public class Day20IntermediateStrings {
             System.out.print(e + " ");
         });
         System.out.println("");
+
+        //AS5 > isAlnum()
+        ArrayList<Character> inputAS5 = new ArrayList<>(Arrays.asList('S', 'c', 'a', 'l', 'e', 'r', '#', '2', '0', '2', '0'));
+        int outputAS5 = isalnum(inputAS5);
+        System.out.println("AS5 - isalnum :" + outputAS5);
+
+        //AS6 > Isalpha()
+        ArrayList<Character> inputAS6 = new ArrayList<>(Arrays.asList('S', 'c', 'a', 'l', 'e', 'r', '#', '2', '0', '2', '0'));
+        int outputAS6 = isalpha(inputAS6);
+        System.out.println("AS6 - isalpha :" + outputAS6);
+
+
+
+
+        //HW1 > Amazing Subarrays
+        String inputHW1 = "ABEC";
+        int outputHW1 = amazingSubarrays(inputHW1);
+        System.out.println("HW1 - Amazing Subarrays :" + outputHW1);
+
+    }
+
+//    I am an Indian
+//    naidnI na ma I
+//
+
+    /**
+     * AS1 > Reverse the String [CONCEPT]- Double reverse
+     * Given a string A of size N. Return the string A after reversing the string word by word.
+     * Example => A = "the sky is blue" => o/p => "blue is sky the"
+     * @param A
+     * @return
+     */
+    private String reverseTheString(String A) {
+        String totalReversedString = reverseAnyString(A);
+        totalReversedString = totalReversedString + " ";
+        StringBuilder builder = new StringBuilder();
+        StringBuilder currentWordBuilder = new StringBuilder();
+        for (int i = 0; i < totalReversedString.length(); i++) {
+            if (totalReversedString.charAt(i) == ' ') {
+                String unreversedWord = reverseAnyString(currentWordBuilder.toString());
+                builder.append(unreversedWord);
+                builder.append(" ");
+                currentWordBuilder = new StringBuilder();
+            } else {
+                currentWordBuilder.append(totalReversedString.charAt(i));
+            }
+        }
+        return builder.toString().trim();
+    }
+
+    private String reverseAnyString(String originalString) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = originalString.length() - 1; i >= 0; i--) {
+            Character currentChar = originalString.charAt(i);
+            builder.append(currentChar);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * AS2 > Simple Reverse [EASY]
+     * Given a string A, you are asked to reverse the string and return the reversed string.
+     * Example => A = "scaler" => o/p => "relacs"
+     * @param A
+     * @return
+     */
+    private String simpleReverse(String A) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = A.length() - 1; i >= 0; i--) {
+            builder.append(A.charAt(i));
+        }
+        return builder.toString();
     }
 
     /**
@@ -64,5 +147,98 @@ public class Day20IntermediateStrings {
             }
         }
         return result;
+    }
+
+    /**
+     * AS5 > Isalnum() [EASY]
+     * Character array A. Return 1 if all the characters of a character array are alphanumeric (a-z, A-Z and 0-9), else return 0.
+     * Example => A = ['S', 'c', 'a', 'l', 'e', 'r', 'A', 'c', 'a', 'd', 'e', 'm', 'y', '2', '0', '2', '0'] => 1
+     * A = ['S', 'c', 'a', 'l', 'e', 'r', '#', '2', '0', '2', '0'] => 0
+     * @param A
+     * @return
+     */
+    private int isalnum(ArrayList<Character> A) {
+        int isAlnum = 1;
+        for (int i = 0; i < A.size(); i++) {
+            Character eachChar = A.get(i);
+            if (!((eachChar >= 'A' && eachChar <= 'Z') || (eachChar >= 'a' && eachChar <= 'z') || (eachChar >= '0' && eachChar <= '9'))) {
+                isAlnum = 0;
+                break;
+            }
+        }
+        return isAlnum;
+    }
+
+    /**
+     * AS6 > Isalpha() [EASY]
+     * Character array A. Return 1 if all the characters of a character array are alphanumeric (a-z, A-Z), else return 0.
+     * @param A
+     * @return
+     */
+    private int isalpha(ArrayList<Character> A) {
+        int isAlpha = 1;
+        for (int i = 0; i < A.size(); i++) {
+            Character eachChar = A.get(i);
+            if (!((eachChar >= 'A' && eachChar <= 'Z') || (eachChar >= 'a' && eachChar <= 'z'))) {
+                isAlpha = 0;
+                break;
+            }
+        }
+        return isAlpha;
+    }
+
+
+
+
+    /**
+     * HW1 > Amazing Subarrays
+     * Given a string S, and you have to find all the amazing substrings of S.
+     * Amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
+     * @param A
+     * @return
+     */
+    private int amazingSubarrays(String A) {
+        int rollingCount = 0;
+        int combinations = 0;
+        for (int i = A.length() - 1; i >= 0; i--) {
+            Character currentChar = A.charAt(i);
+            rollingCount++;
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u'
+                || currentChar == 'A' || currentChar == 'E' || currentChar == 'I' || currentChar == 'O' || currentChar == 'U') {
+                combinations = combinations + rollingCount;
+            }
+        }
+        return combinations;
+    }
+
+    /**
+     * HW2 > Count Occurrences
+     * Find number of occurrences of bob in the string A consisting of lowercase english alphabets.
+     * @param A
+     * @return
+     */
+    private int countOccurrences(String A) {
+        String searchString = "bob";
+
+        return 0;
+    }
+
+    /**
+     * HW3 > Change character
+     * Given a string A of size N consisting of lowercase alphabets. You can change at most B characters in the given
+     * string to any other lowercase alphabet such that the number of distinct characters in the string is minimized.
+     * Find the minimum number of distinct characters in the resulting string.
+     * Example => A = "abcabbccd", B = 3 => o/p => 2 ; We can change both 'a' and one 'd' into 'b'.So the new string becomes "bbcbbbccb".
+     * So the minimum number of distinct character will be 2.
+     * @param A
+     * @param B
+     * @return
+     */
+    private int changeCharacter(String A, int B) {
+        return 0;
+    }
+
+    private String stringOperations(String A) {
+        return "";
     }
 }
