2837b900b7e216e3a97fdbccb8f406f994a35193
IndranilBasu <Indranil.Basu@target.com>
2021-12-26 01:48:47 +0530

    26. Day28_TreesBasic2 - AS1/2/

--- src/com/tutorial/Trees/Day28_TreesBasic2.java
+++ src/com/tutorial/Trees/Day28_TreesBasic2.java
@@ -37,4 +37,56 @@ public class Day28_TreesBasic2 {
         return rightSearch;
     }
 
+
+    /**
+     * AS1 > Identical Binary Trees
+     * Given two binary trees, check if they are equal or not.
+     * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+     * @param A
+     * @param B
+     * @return
+     */
+    private int identicalBinaryTrees(TreeNode A, TreeNode B) {
+        if (A == null && B == null) {
+            return 1;
+        }
+        if ((A == null && B != null) || (A != null && B == null)) {
+            return 0;
+        }
+        if (A.val == B.val) {
+            return identicalBinaryTrees(A.left, B.left) * identicalBinaryTrees(A.right, B.right);
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * AS2 > Symmetric Binary Tree
+     * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+     * @param A
+     * @return
+     */
+    private int isSymmetric(TreeNode A) {
+        return 0;
+    }
+
+    /**
+     * AS3 > BST nodes in a range
+     * Given a binary search tree of integers. You are given a range B and C.
+     * Return the count of the number of nodes that lies in the given range.
+     * @param A
+     * @param B
+     * @param C
+     * @return
+     */
+    private int howManyNodesInRange(TreeNode A, int B, int C) {
+        int count = 0;
+        if (A == null) {
+            return 0;
+        }
+        if (A.val >= B && A.val <= C) {
+            count++;
+        }
+        return count + howManyNodesInRange(A.left, B, C) + howManyNodesInRange(A.right, B, C);
+    }
 }
