fc67630113cedd8f717fc753ca9397bbff47eb0c
IndranilBasu <Indranil.Basu@target.com>
2022-02-09 18:03:43 +0530

    36. buildPrefixSumMatrix

--- src/com/tutorial/Advanced/Day34_Arrays2.java
+++ src/com/tutorial/Advanced/Day34_Arrays2.java
@@ -22,12 +22,21 @@ public class Day34_Arrays2 {
         inputCW2.add(new ArrayList<>(Arrays.asList(8, 4 ,2 ,3, 1)));
         int outputCW2 = this.sumOfSubMatrixBruteForcePrefixSumRow(inputCW2, 1, 1 ,2, 3);
         System.out.println("CW2 > Sum of elemets of submatrix - Brute Force - Optimization 1 - with PS per row: " + outputCW2);
+
+        //CW3 > Build prefix sum matrix
+        ArrayList<ArrayList<Integer>> inputCW3 = new ArrayList<>();
+        inputCW3.add(new ArrayList<>(Arrays.asList(1, 5, 2, 3, 4)));
+        inputCW3.add(new ArrayList<>(Arrays.asList(7, 2 ,3 ,4, 2)));
+        inputCW3.add(new ArrayList<>(Arrays.asList(3, 4 ,2 ,5, 3)));
+        inputCW3.add(new ArrayList<>(Arrays.asList(8, 4 ,2 ,3, 1)));
+        this.buildPrefixSumMatrix(inputCW3);
     }
 
     /**
      * CW1 > Sum of elemets of submatrix - Brute Force
-     * TC = O(n2) - square matrix (n * n)      => For q queries = O(qn2)
-     * TC = O(nm) - rectangular matrix (n * m) => For q queries = O(nmq)
+     * TC [square matrix n * n] = O(n2) => For q queries = O(qn2)
+     * TC [rect matrix n * m]   = O(nm) => For q queries = O(nmq)
+     * SC = O(1)
      * @param A
      * @param r1
      * @param c1
@@ -47,8 +56,9 @@ public class Day34_Arrays2 {
 
     /**
      * CW2 > Sum of elemets of submatrix - Brute Force - Optimization 1 - with PS per row
-     * TC = O(n2) - square matrix (n * n)      => For q queries = O(qn2)
-     * TC = O(nm) - rectangular matrix (n * m) => For q queries = O(nmq)
+     * TC [square matrix n * n] = O(n2) [PS generation] + O(n) [Sum]  => For q queries = O(qn2) + O(qn)
+     * TC [rect matrix n * m]   = O(nm) [PS generation] + O(N) [Sum]  => For q queries = O(nmq) + O(qn)
+     * SC = o(nm)
      * @param A
      * @param r1
      * @param c1
@@ -78,5 +88,43 @@ public class Day34_Arrays2 {
         return sum;
     }
 
+    /**
+     * Prefix sum matrix creation
+     * @param A
+     */
+    private void buildPrefixSumMatrix(ArrayList<ArrayList<Integer>> A) {
+        int rows = A.size();
+        int cols = A.get(0).size();
+        Integer[][] prefixMatrix = new Integer[rows][cols];
 
+        //Building prefix sum matrix
+        //0th row
+        int runningSum = 0;
+        for (int i = 0; i < cols; i++) {
+            runningSum = runningSum + A.get(0).get(i);
+            prefixMatrix[0][i] = runningSum;
+        }
+
+        //0th column
+        runningSum = A.get(0).get(0);
+        for (int j = 1; j < rows; j++) {
+            runningSum = runningSum + A.get(j).get(0);
+            prefixMatrix[j][0] = runningSum;
+        }
+
+        //All other row/cols
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                prefixMatrix[i][j] = prefixMatrix[i-1][j] + prefixMatrix[i][j-1] - prefixMatrix[i-1][j-1] + A.get(i).get(j);
+            }
+        }
+
+        //Display
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(prefixMatrix[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
 }
