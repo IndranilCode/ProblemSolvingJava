22b9a7d63af4e3ee45421728d929f9f99eda7fde
IndranilBasu <Indranil.Basu@target.com>
2022-03-06 23:47:12 +0530

    50. selectionSort

--- src/com/tutorial/AdvancedDSA/M5_Sorting/Day45_Sorting1.java
+++ src/com/tutorial/AdvancedDSA/M5_Sorting/Day45_Sorting1.java
@@ -10,11 +10,16 @@ public class Day45_Sorting1 {
         System.out.println("CW1 > 2nd largest number [5, 2, 7, 12, 99, 6, 1] => " + this.secondLargestElement(inputCW1));
 
         //CW2 > Selection Sort
-
+        ArrayList<Integer> inputCW2 = new ArrayList<>(Arrays.asList(5, 2, 7, 12, 99, 6, 1));
+        ArrayList<Integer> resultCW2 = this.selectionSort(inputCW2);
+        System.out.print("CW2 > Selection sort [5, 2, 7, 12, 99, 6, 1] => ");
+        resultCW2.forEach(e -> System.out.print(e + ", "));
+        System.out.println("");
     }
 
     /**
      * CW1 > 2nd largest number
+     * TC = O(n) , SC = O(1)
      * @param numbers
      * @return
      */
@@ -50,10 +55,28 @@ public class Day45_Sorting1 {
 
     /**
      * CW2 > Selection Sort
+     * TC = O(N2)
      * @param a
      * @return
      */
-    private Integer[] selectionSort(ArrayList<Integer> a) {
-        return null;
+    private ArrayList<Integer> selectionSort(ArrayList<Integer> a) {
+        int currentMax;
+        int currentMaxIndex;
+        int temp;
+        for (int endOfActiveArr = a.size() - 1; endOfActiveArr >= 0; endOfActiveArr--) {
+            currentMax = Integer.MIN_VALUE;
+            currentMaxIndex = -1;
+            for (int i = 0; i <= endOfActiveArr; i++) {
+                if (a.get(i) > currentMax) {
+                    currentMax = a.get(i);
+                    currentMaxIndex = i;
+                }
+            }
+            //Max found => Swap max element with last active index
+            temp = a.get(endOfActiveArr);
+            a.set(endOfActiveArr, currentMax); //Set lastElem = max
+            a.set(currentMaxIndex, temp); //Set index@maxNumber = last element
+        }
+        return a;
     }
 }
