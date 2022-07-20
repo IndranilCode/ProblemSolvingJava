f20f811d6de63ff97742eecb6fe7d91c0790583d
IndranilBasu <Indranil.Basu@target.com>
2022-07-20 16:17:23 +0530

    97. AS3 > Reorder List

--- src/com/tutorial/AdvancedDSA/M10_LinkList/Day59_71_LinkList2.java
+++ src/com/tutorial/AdvancedDSA/M10_LinkList/Day59_71_LinkList2.java
@@ -16,8 +16,21 @@ public class Day59_71_LinkList2 {
         System.out.print("AS2 > Remove Loop from Linked List [Floyd's cycle detection algo] [3 -> 2 -> 4 -> 5 -> 6 -> .....4 (loop) ] => ");
         this.printList(resultAS2);
 
+        //AS3 > Reorder List
+        NodeLList inputAS3_a1_n7 = new NodeLList(7);
+        NodeLList inputAS3_a1_n6 = new NodeLList(6); inputAS3_a1_n6.next = inputAS3_a1_n7;
+        NodeLList inputAS3_a1_n5 = new NodeLList(5); inputAS3_a1_n5.next = inputAS3_a1_n6;
+        NodeLList inputAS3_a1_n4 = new NodeLList(4); inputAS3_a1_n4.next = inputAS3_a1_n5;
+        NodeLList inputAS3_a1_n3 = new NodeLList(3); inputAS3_a1_n3.next = inputAS3_a1_n4;
+        NodeLList inputAS3_a1_n2 = new NodeLList(2); inputAS3_a1_n2.next = inputAS3_a1_n3;
+        NodeLList inputAS3_a1_n1 = new NodeLList(1); inputAS3_a1_n1.next = inputAS3_a1_n2;
+        NodeLList resultAS3_1 = this.reorderList(inputAS3_a1_n1);
+        System.out.print("AS3 > Reorder List [1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7] => ");
+        this.printList(resultAS3_1);
+
         System.out.println("-----------------HOMEWORK----------------");
 
+        //HW2 > Merge Two Sorted Lists
         NodeLList inputHW2_a1_n7 = new NodeLList(27);
         NodeLList inputHW2_a1_n6 = new NodeLList(26); inputHW2_a1_n6.next = inputHW2_a1_n7;
         NodeLList inputHW2_a1_n5 = new NodeLList(23); inputHW2_a1_n5.next = inputHW2_a1_n6;
@@ -42,17 +55,6 @@ public class Day59_71_LinkList2 {
         }
         System.out.println("");
     }
-
-//    private NodeLList reorderLinkList(NodeLList head) {
-//        //Split LL to 2 parts
-//        NodeLList slow = head;
-//        NodeLList fast = head;
-//        while (fast != null && fast.next != null) {
-//            slow = slow.next;
-//            fast = fast.next.next;
-//        }
-//
-//    }
 
 
     /**
@@ -81,6 +83,72 @@ public class Day59_71_LinkList2 {
         return head;
     }
 
+    /**
+     * AS3 > Reorder List
+     * Given a singly linked list A
+     *  A0 → A1 → … → An-1 → An
+     * reorder it to:
+     *  A0 → An → A1 → An-1 → A2 → An-2 → …
+     * You must do this in-place without altering the nodes' values.
+     * @param head
+     * @return
+     */
+    private NodeLList reorderList(NodeLList head) {
+        NodeLList fast = head;
+        NodeLList slow = head;
+
+        if (fast == null || fast.next == null || fast.next.next == null) {
+            return head;
+        }
+        //Find the mid of the LL (even : 1st mid, odd: normal)
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //Split the list into 2 halves at mid
+        NodeLList head2 = slow.next;
+        slow.next = null;
+
+        //Reverse the 2nd list
+        NodeLList h1 = head2;
+        NodeLList h2 = head2.next;
+        h1.next = null;
+        while (h2 != null) {
+            NodeLList temp = h2.next;
+            h2.next = h1;
+            h1 = h2;
+            h2 = temp;
+        }
+        //h1 is new head
+        head2 = h1;
+
+        //Merge the 2 list alternately
+        int runner = 0;
+        h1 = head;
+        h2 = head2;
+        NodeLList t1 = h1.next;
+        NodeLList t2 = h2.next;
+        while (h1 != null && h2 != null) {
+            if (runner % 2 == 0) {
+                h1.next = h2;
+                h1 = t1;
+                if (t1 != null) {
+                    t1 = t1.next;
+                }
+            } else {
+                h2.next = h1;
+                h2 = t2;
+                if (t2 != null) {
+                    t2 = t2.next;
+                }
+            }
+            runner++;
+        }
+        return head;
+    }
+
+    /*---------------HOMEWORK--------------*/
 
     /**
      * HW2 > Merge Two Sorted Lists
@@ -120,5 +188,4 @@ public class Day59_71_LinkList2 {
         }
         return head;
     }
-
 }
